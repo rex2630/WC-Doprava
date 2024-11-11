1b37edd4f4985b4d0924dbb82f685a89835fc411
rex2630 <rex2630@gmail.com>
2024-11-11 16:47:06 +0100

    rest of rebranding

--- components/class-wedo.php
+++ components/class-wedo.php
@@ -4,7 +4,7 @@ class WC_Shipping_doprava_WEDO
 extends WC_Shipping_Method{public function __construct($instance_id=0){
   $this->instance_id=absint($instance_id);
   $this->id='doprava_wedo';
-  $this->method_title='WE|DO';$this->method_description=' Zákazníci si na mapě snadno a pohodlně vyberou nejbližší výdejní místo WE|DO ULOŽENKA nebo WE|DO BOX.';
+  $this->method_title='WE|DO';$this->method_description=' Zákazníci si na mapě snadno a pohodlně vyberou nejbližší výdejní místo ONE POINT nebo ONE BOX.';
   $aktivace_wedo=get_option('wc_doprava_doprava_wedo');$this->enabled=$aktivace_wedo;
   $this->supports=array('shipping-zones','settings','instance-settings','instance-settings-modal',);
   $this->init();
@@ -21,9 +21,9 @@ public function calculate_shipping($package=array()){$rate=array('id'=>$this->ge
 }
 
 public function init_form_fields(){
-  $this->form_fields=array('op1'=>array('title'=>'WE|DO Uloženka','label'=>'Zobrazení se ve widgetu výdejní místa WE|DO Uloženky','type'=>'checkbox','default'=>'yes',),'op2'=>array('title'=>'WE|DO Boxy','label'=>'Zobrazení se ve widgetu WE|DO Boxy','type'=>'checkbox','default'=>'yes',));
+  $this->form_fields=array('op1'=>array('title'=>'ONE','label'=>'Zobrazení se ve widgetu výdejní místa ONE','type'=>'checkbox','default'=>'yes',),'op2'=>array('title'=>'ONE Boxy','label'=>'Zobrazovat ve widgetu ONE Boxy','type'=>'checkbox','default'=>'yes',));
 }
 
 public function init_instance_form_fields(){
-  $this->instance_form_fields=array('wedo_nazev'=>array('title'=>'Název','type'=>'text','description'=>'Název pro zobrazení v eshopu.','default'=>'WE|DO','css'=>'width: 300px;'),'wedo_zakladni-cena'=>array('title'=>'Základní cena','type'=>'price','description'=>'Pokud nebude cena vyplněna, tak bude nulová.','default'=>'','css'=>'width: 100px;','placeholder'=>wc_format_localized_price(0)),);
+  $this->instance_form_fields=array('wedo_nazev'=>array('title'=>'Název','type'=>'text','description'=>'Název pro zobrazení v eshopu.','default'=>'ONE','css'=>'width: 300px;'),'wedo_zakladni-cena'=>array('title'=>'Základní cena','type'=>'price','description'=>'Pokud nebude cena vyplněna, tak bude nulová.','default'=>'','css'=>'width: 100px;','placeholder'=>wc_format_localized_price(0)),);
 }}
